e804cf26033fb64fe3a7f418005cc999a022faba
Kopalmuhamad <muhamadkopal32@gmail.com>
2024-12-06 21:36:10 +0700

    change base ap'i url for testing

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://z-hot.vercel.app/",
+        target: "http://localhost:3000/",
         changeOrigin: true,
       },
     },
